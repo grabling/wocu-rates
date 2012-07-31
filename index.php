db931a3207ff29351a43cc3ae1d894411842d678
Chris Blakey <chris.a.blakey@googlemail.com>
2012-07-31 16:36:37 +0100

    update

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <?php 
-    include 'core/init.php';
-    include 'inc/header.php'; 
+include 'core/init.php';
+include 'inc/header.php'; 
 
-    $currency_1_id = $_POST['c1'];
-    $currency_2_id = $_POST['c2'];
-    $start_date = $_POST['period']
+$currency_1_id = $_POST['c1'];
+$currency_2_id = $_POST['c2'];
+$start_date = $_POST['period']
 ?> 
 
 <h1>Home</h1>
-<?php include 'inc/form.php';?>
+<?php 
+include 'inc/form.php';
 
-<?php
-    if ($_GET['t'] == 'graph') {
-        include 'inc/graph.php';
-    }
+if ($_GET['t'] == 'graph') {
+    include 'inc/graph.php';
+}
 ?>
+
       
 <?php include 'inc/footer.php'; ?>
