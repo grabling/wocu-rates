ded251cc22c92f6b79ff35200c1aabcb73da8bad
Chris <chris.a.blakey@googlemail.com>
2012-10-10 19:47:05 +0100

    Updated function to return currency names

--- core/functions/query.php
+++ core/functions/query.php
@@ -2,7 +2,7 @@
 if(!defined('INCLUDE_CHECK')) die('You are not allowed to execute this file directly');
 
 function get_all_currency_names() {
-	return mysql_query("SELECT * FROM `currency` ORDER BY `name` ASC");
+	return mysql_query("SELECT * FROM `currency`");
 }
 
 function get_currency_name($currency_id) {
