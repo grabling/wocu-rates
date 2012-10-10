fc0293ae1bbd303526d2ac6250204d9f89326140
Chris <chris.a.blakey@googlemail.com>
2012-10-10 18:37:50 +0100

    DB

--- core/database/connect.php
+++ core/database/connect.php
@@ -4,9 +4,15 @@ if(!defined('INCLUDE_CHECK')) die('You are not allowed to execute this file dire
 
 /* Database config */
 
+// $hostname = 'localhost';
+// $username='root';
+// $password='root';
+// $dbname='wocu';
+
+
 $hostname = 'localhost';
 $username='root';
-$password='root';
+$password='aEE8vkyp';
 $dbname='wocu';
 
 /* End config */
