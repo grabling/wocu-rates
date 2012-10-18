450f5f7bbe815361a12adc94d3f7a740c4cc14d4
grabling <mike@grabling.com>
2012-10-18 16:33:07 +0200

    Update inc/header.php

--- inc/header.php
+++ inc/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
   <meta charset="utf-8">
-  <title><?php echo $site_name; ?></title>
+  <title><?php echo get_min_date(); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="">
   <meta name="author" content="">
