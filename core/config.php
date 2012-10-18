a49ecc4102d97eb8cf8ff6106d40a499982f5790
grabling <mike@grabling.com>
2012-10-18 16:50:46 +0200

    Update core/config.php

--- core/config.php
+++ core/config.php
@@ -5,7 +5,7 @@ if(!defined('INCLUDE_CHECK')) die('You are not allowed to execute this file dire
 /* Config */
 
 $site_name		= 'xWOCU';
-$company_name	= 'xWOCU';	
+$company_name	= 'xWOCU'.get_max_date();;	
 
 /* End config */
 
