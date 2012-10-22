5994dc5dbd4687fddb8f9abc9829253948778113
grabling <mike@grabling.com>
2012-10-22 12:48:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ $end_date     = $_REQUEST['enddate'];
 $graph_type     = $_REQUEST['type'];
  
 include 'inc/form.php';
-include 'inc/form2.php';
 
 if (isset($_REQUEST['c1'])) {
     include 'inc/graph.php';
