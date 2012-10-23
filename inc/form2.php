0df7c5a396407748d973bf2d39b71bb8c763f625
mike blakey <mike@grabling.com>
2012-10-23 15:44:06 +0200

    Update inc/form2.php

--- inc/form2.php
+++ inc/form2.php
@@ -1,5 +1,6 @@
 <div class="well span3">
 	<div class="row-fluid">
+	   <h1>
 		<form action="index.php" method="GET">
 			<div class="span12">
 				<div class="span12"><h1> <?php echo 'Volatility since '.$start_date; ?></h1></div>
@@ -17,6 +18,7 @@
 				<div class="span6"><h1>76.50%</h1></div>
 			</div>
 		</form>
+	   </h1>
 	</div>
 </div>
 
