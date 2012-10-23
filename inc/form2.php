d991bc4c19bd266a7539daadf110075a144a1b79
mike blakey <mike@grabling.com>
2012-10-23 15:47:07 +0200

    Full font formatting

--- inc/form2.php
+++ inc/form2.php
@@ -1,24 +1,30 @@
 <div class="well span3">
 	<div class="row-fluid">
-	   <h1>
 		<form action="index.php" method="GET">
 			<div class="span12">
-				<div class="span12"><h1> <?php echo 'Volatility since '.$start_date; ?></h1></div>
+			   <h1>
+				<div class="span12"><?php echo 'Volatility since '.$start_date; ?></div>
+			   </h1>
 			</div>
 			<div class="span12">	
+			   <h2>
 				<div class="span6"> <?php echo get_currency_name($currency_1_id) .'/'. get_currency_name($currency_2_id); ?></div>
 				<div class="span6">55%</div>
+			   </h2>
 			</div>
 			<div class="span12">
+			   <h2>
 				<div class="span6"> <?php echo 'XCU/'. get_currency_name($currency_2_id); ?></div>
 				<div class="span6">8.85%</div>
+			   </h2>
 			</div>
 			<div class="span12">
-				<div class="span6"><h1>Reduction</h1></div>
-				<div class="span6"><h1>76.50%</h1></div>
+			   <h1>
+				<div class="span6">Reduction</div>
+				<div class="span6">50%</div>
+			   </h1>
 			</div>
 		</form>
-	   </h1>
 	</div>
 </div>
 
