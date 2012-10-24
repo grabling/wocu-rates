26f63ae973d07e28c8e92a4b4df929e95c90cd2d
grabling <mike@grabling.com>
2012-10-24 13:49:05 +0100

    Added DB Query call to add STDDEV values

--- inc/form2.php
+++ inc/form2.php
@@ -2,27 +2,19 @@
 	<div class="row-fluid">
 		<form action="index.php" method="GET">
 			<div class="span12">
-			   <h1>
 				<div class="span12"><?php echo 'Volatility since '.$start_date; ?></div>
-			   </h1>
 			</div>
 			<div class="span12">	
-			   <h1>
-				<div class="span6"> <?php echo get_currency_name($currency_1_id) .'/'. get_currency_name($currency_2_id); ?></div>
-				<div class="span6">55%</div>
-			   </h1>
+				<div class="span6"><?php echo get_currency_name($currency_1_id) .'/'. get_currency_name($currency_2_id);?></div>
+				<div class="span6"><?php echo calculation_stddev($currency_1_id, $currency_2_id, $start_date, $end_date));?></div>
 			</div>
 			<div class="span12">
-			   <h1>
-				<div class="span6"> <?php echo 'XCU/'. get_currency_name($currency_2_id); ?></div>
-				<div class="span6">8.85%</div>
-			   </h1>
+				<div class="span6"><?php echo 'XCU/'. get_currency_name($currency_2_id); ?></div>
+				<div class="span6"><?php echo calculation_stddev(get_wocu_id(), $currency_2_id, $start_date, $end_date));?></div>
 			</div>
 			<div class="span12">
-			   <h2>
 				<div class="span6">Reduction</div>
 				<div class="span6">50%</div>
-			   </h2>
 			</div>
 		</form>
 	</div>
