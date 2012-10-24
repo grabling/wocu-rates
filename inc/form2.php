eb38df1bbd86158e4201441043d9837c53d26b01
grabling <mike@grabling.com>
2012-10-24 13:59:32 +0100

    Corrected syntax in DB Query

--- inc/form2.php
+++ inc/form2.php
@@ -1,20 +1,24 @@
 <div class="well span3">
 	<div class="row-fluid">
 		<form action="index.php" method="GET">
+			<?php
+			$result1 = calculation_stddev($currency_1_id, $currency_2_id, $start_date, $end_date));
+			$result2 = calculation_stddev(get_wocu_id(), $currency_2_id, $start_date, $end_date));
+			?>
 			<div class="span12">
 				<div class="span12"><?php echo 'Volatility since '.$start_date; ?></div>
 			</div>
 			<div class="span12">	
 				<div class="span6"><?php echo get_currency_name($currency_1_id) .'/'. get_currency_name($currency_2_id);?></div>
-				<div class="span6"><?php echo calculation_stddev($currency_1_id, $currency_2_id, $start_date, $end_date));?></div>
+				<div class="span6"><?php echo $result1;?></div>
 			</div>
 			<div class="span12">
 				<div class="span6"><?php echo 'XCU/'. get_currency_name($currency_2_id); ?></div>
-				<div class="span6"><?php echo calculation_stddev(get_wocu_id(), $currency_2_id, $start_date, $end_date));?></div>
+				<div class="span6"><?php echo $result2;?></div>
 			</div>
 			<div class="span12">
 				<div class="span6">Reduction</div>
-				<div class="span6">50%</div>
+				<div class="span6"><?php echo ($result1 - $result2) / $result1;?></div>
 			</div>
 		</form>
 	</div>
