5f78a2f8c9f3b5df3e89443806cd5d52b95e356b
grabling <mike@grabling.com>
2012-10-24 14:27:57 +0100

    again

--- inc/form2.php
+++ inc/form2.php
@@ -1,10 +1,10 @@
 <div class="well span3">
 	<div class="row-fluid">
 		<form action="index.php" method="GET">
-			<?php
-			$result1 = calculation_stddev($currency_1_id, $currency_2_id, $start_date, $end_date);
-			$result2 = calculation_stddev(get_wocu_id(), $currency_2_id, $start_date, $end_date);
-			?>
+			
+			$result1 = <?php calculation_stddev($currency_1_id, $currency_2_id, $start_date, $end_date); ?>
+			$result2 = <?php calculation_stddev(get_wocu_id(), $currency_2_id, $start_date, $end_date); ?>
+			
 			<div class="span12">
 				<div class="span12"><?php echo 'Volatility since '.$start_date; ?></div>
 			</div>
