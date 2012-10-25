60958974e73222915e337e7a9d6ce31ea9716a82
grabling <mike@grabling.com>
2012-10-25 11:44:50 +0100

    Dispaly STDDEV result as %

--- inc/form2.php
+++ inc/form2.php
@@ -7,12 +7,12 @@
 			<div class="span12">	
 				<div class="span6"><?php echo get_currency_name($currency_1_id) .'/'. get_currency_name($currency_2_id);?></div>
 				<?php $result1 = calculation_stddev($currency_1_id, $currency_2_id, $start_date, $end_date); ?>
-				<div class="span6"><?php echo $result1;?></div>
+				<div class="span6"><?php echo $result1 .'%' ; ?></div>
 			</div>
 			<div class="span12">
 				<div class="span6"><?php echo 'XCU/'. get_currency_name($currency_2_id); ?></div>
 				<?php $result2 = calculation_stddev(get_wocu_id(), $currency_2_id, $start_date, $end_date); ?>
-				<div class="span6"><?php echo $result2;?></div>
+				<div class="span6"><?php echo $result2 .'%' ; ?></div>
 			</div>
 			<div class="span12">
 				<div class="span6">Reduction</div>
