e7e30157e48ef68bc4915475cc1fcac1fc663b36
grabling <mike@grabling.com>
2012-11-21 10:20:42 +0000

    Layout change

--- inc/form2.php
+++ inc/form2.php
@@ -1,22 +1,24 @@
-<div class="well span3">
+<div class="well">
 	<div class="row-fluid">
 		<form action="index.php" method="GET">
 			<div class="span12">
-				<div class="span12"><?php echo 'Volatility since '.$start_date; ?></div>
-			</div>
-			<div class="span12">	
-				<div class="span6"><?php echo get_currency_name($currency_1_id) .'/'. get_currency_name($currency_2_id);?></div>
-				<?php $result1 = calculation_stddev($currency_1_id, $currency_2_id, $start_date, $end_date); ?>
-				<div class="span6"><?php echo $result1 .'%' ; ?></div>
-			</div>
-			<div class="span12">
-				<div class="span6"><?php echo 'XCU/'. get_currency_name($currency_2_id); ?></div>
-				<?php $result2 = calculation_stddev(get_wocu_id(), $currency_2_id, $start_date, $end_date); ?>
-				<div class="span6"><?php echo $result2 .'%' ; ?></div>
-			</div>
-			<div class="span12">
-				<div class="span6">Reduction</div>
-				<div class="span6"><?php echo round(((($result1 - $result2) / $result1)*100),2) .'%' ;?></div>
+				<div class="span3">
+					<?php echo 'Volatility since '.$start_date; ?>
+				</div>
+				<div class="span3">
+					<?php echo get_currency_name($currency_1_id) .'/'. get_currency_name($currency_2_id);?>
+					<?php $result1 = calculation_stddev($currency_1_id, $currency_2_id, $start_date, $end_date); ?>
+					<?php echo ' '. $result1 .'%' ; ?>
+				</div>
+				<div class="span3">
+					<?php echo 'XCU/'. get_currency_name($currency_2_id); ?>
+					<?php $result2 = calculation_stddev(get_wocu_id(), $currency_2_id, $start_date, $end_date); ?>
+					<?php echo ' '. $result2 .'%' ; ?>
+				</div>
+				<div class="span3">
+					<?php echo 'Reduction '; ?>
+					<?php echo round(((($result1 - $result2) / $result1)*100),2) .'%' ;?>
+				</div>
 			</div>
 		</form>
 	</div>
