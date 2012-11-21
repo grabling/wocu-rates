7bb9a353214d46a983598e98fa972459907875f0
grabling <mike@grabling.com>
2012-11-21 10:34:55 +0000

    layout change

--- inc/form2.php
+++ inc/form2.php
@@ -2,21 +2,15 @@
 	<div class="row-fluid">
 		<form action="index.php" method="GET">
 			<div class="span12">
-				<div class="span3">
-					<?php echo 'Volatility since '.$start_date; ?>
-				</div>
-				<div class="span3">
+				<div class="span12">
+					<?php echo 'Volatility since '.$start_date.': '; ?>
 					<?php echo get_currency_name($currency_1_id) .'/'. get_currency_name($currency_2_id);?>
 					<?php $result1 = calculation_stddev($currency_1_id, $currency_2_id, $start_date, $end_date); ?>
-					<?php echo ' '. $result1 .'%' ; ?>
-				</div>
-				<div class="span3">
-					<?php echo 'XCU/'. get_currency_name($currency_2_id); ?>
+					<?php echo ' = '. $result1 .'%' ; ?>
+					<?php echo ' XCU/'. get_currency_name($currency_2_id); ?>
 					<?php $result2 = calculation_stddev(get_wocu_id(), $currency_2_id, $start_date, $end_date); ?>
-					<?php echo ' '. $result2 .'%' ; ?>
-				</div>
-				<div class="span3">
-					<?php echo 'Reduction '; ?>
+					<?php echo ' = '. $result2 .'%' ; ?>
+					<?php echo ' Reduction = '; ?>
 					<?php echo round(((($result1 - $result2) / $result1)*100),2) .'%' ;?>
 				</div>
 			</div>
